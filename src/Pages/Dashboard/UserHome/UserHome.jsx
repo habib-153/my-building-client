1f999a544fda098d873b42a1360e04d91f3d4d16
habib-153 <h.r.sihab155@gmail.com>
2023-11-27 20:31:16 +0600

    userHome

---
+++ src/Pages/Dashboard/UserHome/UserHome.jsx
@@ -0,0 +1,35 @@
+import useAuth from "../../../Hooks/useAuth";
+
+const UserHome = () => {
+  const { user } = useAuth();
+  return (
+    <div>
+      <h2 className="flex gap-3 items-center">
+        <span>Hi, Welcome </span>
+        <div className="text-2xl font-semibold">
+          {user?.displayName ? user.displayName : "Back"}
+        </div>
+      </h2>
+      <div className="w-full">
+        <div className="card mx-auto bg-base-100 shadow-xl">
+          <figure className="w-full">
+            <img className="w-[250px] rounded-full"
+              src={user.photoURL}
+              alt="img"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Name: {user.displayName}
+            </h2>
+            <p>Email: {user.email}</p>
+            <p>Agreement accept date: None</p>
+            <p>Rented Apartment Info: None</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserHome;
