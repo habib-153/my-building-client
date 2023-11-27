1f728406d2046840dc1df3e18ae38a24d33a0e82
habib-153 <h.r.sihab155@gmail.com>
2023-11-27 16:53:39 +0600

    pagination on apartment page

--- src/Comnonent/Card/Card.jsx
+++ src/Comnonent/Card/Card.jsx
@@ -4,74 +4,84 @@ import Swal from "sweetalert2";
 import useAuth from "../../Hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
-import useCart from "../../Hooks/useCart";
 
-const Card = ({item}) => {
-    const {apartment_No ,block_name, rent, floor_No,image, _id} = item
-    const {user} = useAuth()
-    const navigate = useNavigate()
-    const location = useLocation()
-    const axiosSecure = useAxiosSecure()
-    const [, refetch] = useCart()
+const Card = ({ item }) => {
+  const { apartment_No, block_name, rent, floor_No, image } = item;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure();
 
-    const handleAddToCart = food =>{
-      console.log(user.email, food)
-      if(user && user.email){
-        const cartItem = {
-          menuId : _id,
-          email: user.email,
-          
+  const handleAgreement = (apartment) => {
+    console.log(user.email, apartment);
+    if (user && user.email) {
+      const requestedApartment = {
+        userName: user.displayName,
+        userEmail: user.email,
+        Floor_no: apartment.floor_No,
+        Block_name: apartment.block_name,
+        Apartment_no: apartment.apartment_No,
+        Rent: apartment.rent,
+        Status: 'pending'
+      };
+      // console.log(requestedApartment)
+      axiosSecure.post("/agreementRequests", requestedApartment).then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            //position: "top-end",
+            icon: "success",
+            title: `Agreement Request Successful. You will be notified soon`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
-        axiosSecure.post('/carts', cartItem)
-        .then(res => {
-          console.log(res.data)
-          if(res.data.insertedId){
-            Swal.fire({
-              //position: "top-end",
-              icon: "success",
-              title: ` added to cart`,
-              showConfirmButton: false,
-              timer: 1500
-            });
-            //refetch cart to update the cart items
-            refetch()
-          }
-        })
-      }
-      else{
-        Swal.fire({
-          title: "You are not Logged In",
-          text: "Please login to add to the Cart",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Login"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            navigate('/login', {state: {from: location}})
-          }
-        });
-      }
+      });
+    } else {
+      Swal.fire({
+        title: "You are not Logged In",
+        text: "Please login to add to the Cart",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
     }
-    return (
-        <div className="card bg-base-100 shadow-xl">
-  <figure><img src={image}  /></figure>
-  <div className="card-body flex flex-col">
-    <h2 className="card-title">Apartment NO: {apartment_No}</h2>
-    <div className="flex">
-    <p>Block: <span className="font-bold">{block_name}</span></p>
-    <p>Floor: <span className="font-bold">{floor_No}</span></p>
-    <p>Rent: <span className="font-bold">${rent}</span></p>
+  };
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="h-[70%]">
+        <img className="h-full" src={image} />
+      </figure>
+      <div className="card-body flex flex-col">
+        <h2 className="card-title">Apartment NO: {apartment_No}</h2>
+        <div className="flex">
+          <p>
+            Block: <span className="font-bold">{block_name}</span>
+          </p>
+          <p>
+            Floor: <span className="font-bold">{floor_No}</span>
+          </p>
+          <p>
+            Rent: <span className="font-bold">${rent}</span>
+          </p>
+        </div>
+        <div className="card-actions justify-end">
+          <button
+            onClick={() => handleAgreement(item)}
+            className="btn bg-[#64b6dfec] hover:bg-[#64b6dfec] border-0 border-b-4 border-[#21526bec]"
+          >
+            Agreement
+          </button>
+        </div>
+      </div>
     </div>
-    <div className="card-actions justify-end">
-    <button 
-    onClick={() => handleAddToCart(item)}
-    className="btn bg-[#64b6dfec] hover:bg-[#64b6dfec] border-0 border-b-4 border-[#21526bec]">Agreement</button>
-    </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Card;
--- src/Pages/Apartment/Apartment.jsx
+++ src/Pages/Apartment/Apartment.jsx
@@ -1,19 +1,65 @@
 import { Helmet } from "react-helmet-async";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import { useQuery } from "@tanstack/react-query";
+import { GrNext, GrPrevious } from "react-icons/gr";
 import Cover from "../Shared/Cover";
 import apartCover from "../../assets/assets/others/5.webp";
 import Card from "../../Comnonent/Card/Card";
 import SectionTitle from "../../Comnonent/SectionTitle/SectionTitle";
+import { useState } from "react";
+import { useEffect } from "react";
+import './pagination.css'
+import useAuth from "../../Hooks/useAuth";
+
 const Apartment = () => {
   const axiosPublic = useAxiosPublic();
-  const { data: apartments = [] } = useQuery({
-    queryKey: ["apartment"],
-    queryFn: async () => {
-      const res = await axiosPublic.get("/apartment");
-      return res.data;
-    },
-  });
+  const [apartments, setApartments] = useState([])
+  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState(0)
+  const [itemsPerPage, setItemPerPage] = useState(6);
+  const numberOfPages = Math.ceil(count/itemsPerPage)
+  const pages = [...Array(numberOfPages).keys()]
+  const {loading} = useAuth()
+
+  const url = `/apartment?page=${currentPage}&size=${itemsPerPage}`
+  useEffect(()=>{
+    fetch('http://localhost:5000/apartmentsCount')
+    .then(res => res.json())
+    .then(data=> setCount(data.count))
+},[])
+
+
+useEffect(()=>{
+  axiosPublic.get(url)
+  .then(res =>{
+    setApartments(res.data)
+  })
+},[axiosPublic, currentPage, itemsPerPage, url])
+
+  const handleItemPerPage = e =>{
+    const value = parseInt(e.target.value)
+    console.log(value)
+    setItemPerPage(value)
+    setCurrentPage(0)
+}
+
+const handlePrev =()=>{
+    if (currentPage>0){
+        setCurrentPage(currentPage -1)
+    }
+}
+
+const handleNext =()=>{
+    if (currentPage < pages.length-1){
+        setCurrentPage(currentPage+1)
+    }
+}
+if(loading){
+  return <div className="text-5xl w-full text-center">
+    <span className="loading my-[20%] loading-dots loading-lg"></span>
+   
+    </div>
+}
+
   return (
     <div className="">
       <Helmet>
@@ -26,13 +72,31 @@ const Apartment = () => {
       ></Cover>
       <div className="my-12">
         <SectionTitle heading="All Apartments"></SectionTitle>
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="my-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {
             apartments?.map(apartment => <Card 
                 item={apartment}
-                key={apartment}></Card>)
+                key={apartment._id}></Card>)
         }
         </div>
+        <div className='pagination'>
+                <p>CurrentPage: {currentPage+1}</p>
+                <button className="bg-black text-white font-bold rounded-full" onClick={handlePrev}><GrPrevious /></button>
+                {
+                    pages.map(page =><button 
+                        className={currentPage === page ? 'selected': undefined}
+                        onClick={()=>setCurrentPage(page)}
+                        key={page}
+                        >{page + 1}</button>)
+                }
+                <button className="bg-black text-white font-bold rounded-full" onClick={handleNext}><GrNext></GrNext></button>
+                <select className="bg-slate-300 p-2 rounded-lg" value={itemsPerPage} onChange={handleItemPerPage} name="" id="">
+                    <option value="6">6</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
+            </div>
       </div>
     </div>
   );
