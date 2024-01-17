ef90af65faa91b7ef9c55c88611d28d835762a3f
habib-153 <h.r.sihab155@gmail.com>
2024-01-17 16:01:03 +0600

    change on register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
-import loginImg from '../../assets/assets/others/authentication.gif';
-import { useContext, } from "react";
+import loginImg from "../../assets/assets/others/authentication.gif";
+import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
@@ -8,74 +8,70 @@ import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import SocialLogin from "../../Comnonent/SocialLogin/SocialLogin";
 import { Helmet } from "react-helmet-async";
 
-const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY
-const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const Register = () => {
-    const axiosPublic = useAxiosPublic()
-    const {createUser, updateUserProfile} = useContext(AuthContext)
-    const navigate = useNavigate()
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-      } = useForm()
+  const axiosPublic = useAxiosPublic();
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = async (data) => {
-        console.log(data)
-        const imageFile = { image: data.photoURL[0]}
-        const res = await axiosPublic.post(image_hosting_api, imageFile,{
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    })
-    
-      const imageURL = res.data.data.display_url
-    console.log(imageURL)
-        createUser(data.email, data.password)
-        .then(res =>{
-            const user = res.user
-            console.log(user)
-            updateUserProfile(data.name, imageURL)
-            .then(()=>{
-              // console.log("User Updated Successfully")
-              const userInfo = {
-                name: data.name,
-                email: data.email
-              }
-              axiosPublic.post('/users', userInfo)
-              .then(res =>{
-                if(res.data.insertedId){
-                  reset()
-              Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "User Created Successfully",
-                showConfirmButton: false,
-                timer: 1500
-              });
-              navigate('/')
-                }
-              })
-              
-            })
-        })
-      }
+  const onSubmit = async (data) => {
+    console.log(data);
+    const imageFile = { image: data.photoURL[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
 
-    return (
-        <div className="hero min-h-screen pt-12">
-          <Helmet>
-                <title>My Building | Register</title>
-            </Helmet>
+    const imageURL = res.data.data.display_url;
+    console.log(imageURL);
+    createUser(data.email, data.password).then((res) => {
+      const user = res.user;
+      console.log(user);
+      updateUserProfile(data.name, imageURL).then(() => {
+        // console.log("User Updated Successfully")
+        const userInfo = {
+          name: data.name,
+          email: data.email,
+        };
+        axiosPublic.post("/users", userInfo).then((res) => {
+          if (res.data.insertedId) {
+            reset();
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "User Created Successfully",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            navigate("/");
+          }
+        });
+      });
+    });
+  };
+
+  return (
+    <div className="hero min-h-screen pt-12">
+      <Helmet>
+        <title>My Building | Register</title>
+      </Helmet>
       <div className="hero-content flex-col md:flex-row lg:gap-12">
         <div className="">
           <img className="rounded-xl" src={loginImg} alt="" />
         </div>
         <div className="card w-full shadow-2xl bg-base-100">
-        <h1 className="text-5xl font-bold text-center">Register</h1>
+          <h1 className="text-5xl font-bold text-center">Register</h1>
           <form onSubmit={handleSubmit(onSubmit)} className="card-body pb-4">
-          <div className="form-control">
+            <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
@@ -84,9 +80,10 @@ const Register = () => {
                 placeholder="Name"
                 {...register("name", { required: true })}
                 className="input input-bordered"
-              />{errors.name && <span>This field is required</span>}
+              />
+              {errors.name && <span>This field is required</span>}
             </div>
-          <div className="form-control">
+            <div className="form-control">
               <label className="label">
                 <span className="label-text">Photo Url</span>
               </label>
@@ -95,7 +92,8 @@ const Register = () => {
                 placeholder="photoUrl"
                 {...register("photoURL", { required: true })}
                 className="input input-bordered pt-2"
-              />{errors.photoURL && <span>This field is required</span>}
+              />
+              {errors.photoURL && <span>This field is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
@@ -106,7 +104,8 @@ const Register = () => {
                 placeholder="email"
                 {...register("email", { required: true })}
                 className="input input-bordered"
-              />{errors.email && <span>This field is required</span>}
+              />
+              {errors.email && <span>This field is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
@@ -115,16 +114,29 @@ const Register = () => {
               <input
                 type="password"
                 placeholder="password"
-                {...register("password", { required: true, 
-                    minLength:6, 
-                    maxLength:20,
-                    pattern:/(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
                 })}
                 className="input input-bordered"
-              />{errors.password?.type === 'required' && <span>This field is required</span>}
-              {errors.password?.type === 'minLength' && <span>Password must be 6 character</span>}
-              {errors.password?.type === 'maxLength' && <span>Password must be less than 20 character</span>}
-              {errors.password?.type === 'pattern' && <span>Password must have one uppercase, one lowercase, one number and one special character</span>}
+              />
+              {errors.password?.type === "required" && (
+                <span>This field is required</span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span>Password must be 6 character</span>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <span>Password must be less than 20 character</span>
+              )}
+              {errors.password?.type === "pattern" && (
+                <span>
+                  Password must have one uppercase, one lowercase, one number
+                  and one special character
+                </span>
+              )}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
@@ -144,16 +156,25 @@ const Register = () => {
               />
               <button onClick={handleValidateCaptcha} className="btn btn-outline btn-xs mt-2">Validate</button>
             </div> */}
-        <p>Already Registered? Please <Link to='/login' className='text-orange-600 font-semibold'>Login</Link></p>
+            <p>
+              Already Registered? Please{" "}
+              <Link to="/login" className="text-orange-600 font-semibold">
+                Login
+              </Link>
+            </p>
             <div className="form-control mt-6">
-              <input  className="btn btn-primary" type="submit" value="Register" />
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Register"
+              />
             </div>
           </form>
           <SocialLogin></SocialLogin>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Register;
